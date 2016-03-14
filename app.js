bf111456b05e480aab8c49c5c282169f0645a787
OsamuMORIMOTO <2525morimoto@gmail.com>
2016-03-14 16:30:38 +0000

    include remarkable.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var http = require('http');
+var Remarkable = require('remarkable');
+var md = new Remarkable();
+
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(md.render('# Remarkable rulezz!'));
     res.end('Hello Woorld\n');
 }).listen(1337);
 console.log('Server running at http://127.0.0.1:1337/');
---
+++ test.md
@@ -0,0 +1,6 @@
+# test markdown
+
+## first section
+
+**hogehoge**
+
