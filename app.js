5b63d222fad467d419127e89d4368690c4a2039f
OsamuMORIMOTO <2525morimoto@gmail.com>
2016-03-16 17:16:08 +0000

    Include ECT (Template Engine)

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var http = require('http');
+
 var Remarkable = require('remarkable');
 var md = new Remarkable();
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(md.render('# Remarkable rulezz!'));
-    res.end('Hello Woorld\n');
-}).listen(1337);
-console.log('Server running at http://127.0.0.1:1337/');
+var fs = require('fs');
+
+var ect = require('ect');
+var renderer = ect({ root : './'});
+
+
+fs.readFile('./test.md', 'utf8', function ( err, text) {
+  if (text != null) {
+    data = { 
+             title : 'Hello, ect world!',
+             md : md.render(text)
+           };
+    http.createServer(function (req, res) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(renderer.render('templates.ect', data));
+      res.end('Hello Woorld\n');
+    }).listen(3000);
+    console.log('Server running');
+  }
+});
+
 
