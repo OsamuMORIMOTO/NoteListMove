6e660dddc7430db24996964a53b20660eafd453f
OsamuMORIMOTO <2525morimoto@gmail.com>
2016-03-17 17:34:41 +0000

    Include Mongodb

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ var fs = require('fs');
 var ect = require('ect');
 var renderer = ect({ root : './'});
 
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/myproject';
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  findDocuments(db, function() {
+//  insertDocuments(db, function() {
+    db.close();
+  });
+});
 
 fs.readFile('./test.md', 'utf8', function ( err, text) {
   if (text != null) {
@@ -24,4 +37,27 @@ fs.readFile('./test.md', 'utf8', function ( err, text) {
   }
 });
 
+var insertDocuments = function(db, callback) {
+  var collection = db.collection('documents');
 
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err,null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted OK");
+    callback(result);
+  });
+}
+
+var findDocuments = function(db, callback) {
+  var collection = db.collection('documents');
+
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found OK");
+    console.dir(docs);
+    callback(docs);
+  });
+}
